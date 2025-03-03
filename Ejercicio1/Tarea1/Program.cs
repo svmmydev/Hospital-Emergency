93fa2a9e61810b6d8b9fd041abe4ef49c8e56757
Sammy <svmmydev@gmail.com>
2025-03-03 01:25:46 +0100

    Base structure made

--- Ejercicio1/Tarea1/Program.cs
+++ Ejercicio1/Tarea1/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿class Doctor(int id)
 {
+    public int Id { get; set; } = id;
+    public bool IsAvailable {get; set;} = true;
+}
+
+internal class Program
+{
+    static int totalPatients = 4;
+    static List<Doctor> availableDoctors =
+    [
+        new Doctor(1),
+        new Doctor(2),
+        new Doctor(3),
+        new Doctor(4)
+    ];
+
+    static SemaphoreSlim consultSem = new SemaphoreSlim(4);
+    static readonly object locker = new object();
+    static Random rnd = new Random();
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Code
     }
 }

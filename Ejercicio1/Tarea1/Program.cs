96e80b5e5866c0019aafc1c76372ca6a8f436613
Sammy <svmmydev@gmail.com>
2025-03-17 00:50:28 +0100

    Var names changed and other fixes. Tarea 3 completed.

--- Ejercicio1/Tarea1/Program.cs
+++ Ejercicio1/Tarea1/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using hospital_urgencias.Models;
+using HospitalUrgencias.Models;
 
 /// <summary>
 /// Simulates a concurrent medical consultation system.
@@ -7,18 +7,7 @@ using hospital_urgencias.Models;
 internal class Program
 {
     static readonly int totalPatients = 4;
-    static readonly List<Doctor> doctors = new List<Doctor>
-    {
-        new Doctor(1),
-        new Doctor(2),
-        new Doctor(3),
-        new Doctor(4)
-    };
-
-
     static readonly SemaphoreSlim consultSem = new SemaphoreSlim(4);
-    static readonly object locker = new object();
-    static readonly Random rnd = new Random();
 
 
     /// <summary>
@@ -26,15 +15,15 @@ internal class Program
     /// </summary>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Patients are entering the hospital..\n");
+        Console.WriteLine("\nPatients are entering the hospital..\n");
 
         // Simulates the arrival of patients at intervals.
         for (int i = 1; i <= totalPatients; i++)
         {
-            int id = i;
+            int arrivalOrderNumber = i;
 
-            Thread pacient = new Thread(() => PatientArrival(id));
-            pacient.Start();
+            Thread patient = new Thread(() => PatientArrival(arrivalOrderNumber, 10000));
+            patient.Start();
 
             Thread.Sleep(2000);
         }
@@ -44,60 +33,20 @@ internal class Program
     /// <summary>
     /// Simulates the arrival of a patient and assigns them to a doctor for a consultation.
     /// </summary>
-    /// <param name="id">The unique identifier of the arriving patient.</param>
-    private static void PatientArrival(int id)
+    /// <param name="PatientId">The unique identifier of the arriving patient.</param>
+    /// <param name="ConsultationTime">Duration of the consultation.</param>
+    private static void PatientArrival(int arrivalOrderNumber, int ConsultationTime)
     {
-        Console.WriteLine($"Patient has arrived. ID: {id}.");
+        Console.WriteLine($"Patient has arrived. Arrival order number: {arrivalOrderNumber}.");
 
         consultSem.Wait();
-        Doctor assignedDoctor = AssignDoctor();
-        Console.WriteLine($"Patient with ID: {id} is actually entering a consult with Doctor {assignedDoctor.Id}.\n");
+        Doctor assignedDoctor = Doctor.AssignDoctor();
 
-        Thread.Sleep(10000);
+        Thread.Sleep(ConsultationTime);
 
-        ReleaseDoctor(assignedDoctor, id);
+        Console.WriteLine($"Patient with arrival order number: {arrivalOrderNumber} has finished the consultation.");
+        assignedDoctor.ReleaseDoctor();
         consultSem.Release();
     }
 
-
-    /// <summary>
-    /// Selects a random available doctor.
-    /// This method blocks execution until a doctor becomes available.
-    /// Access to the doctir list is synchronized to ensure data consistency and avoid race conditions.
-    /// </summary>
-    /// <returns>A Doctor object representing the assigned doctor.</returns>
-    private static Doctor AssignDoctor()
-    {
-        Doctor selectedDoctor;
-
-        while (true)
-        {
-            lock (locker)
-            {
-                var availableDoctors = doctors.Where(d => d.IsAvailable).ToList(); 
-
-                if (availableDoctors.Count > 0)
-                {
-                    selectedDoctor = availableDoctors[rnd.Next(availableDoctors.Count)];
-                    selectedDoctor.IsAvailable = false;
-                    return selectedDoctor;
-                }
-            }
-        }
-    }
-
-
-    /// <summary>
-    /// Marks the doctor as available after a patient finishes their consultation.
-    /// </summary>
-    /// <param name="assignedDoctor">The doctor who was assigned to the patient.</param>
-    /// <param name="patientId">The unique identifier of the patient who has finished the consultation.</param>
-    private static void ReleaseDoctor(Doctor assignedDoctor, int patientId)
-    {
-        lock (locker)
-        {
-            assignedDoctor.IsAvailable = true;
-            Console.WriteLine($"Patient {patientId} has finished the consultation and the Doctor {assignedDoctor.Id} is now free");
-        }
-    }
 }
--- Ejercicio1/Tarea3/Program.cs
+++ Ejercicio1/Tarea3/Program.cs
@@ -1,7 +1,53 @@
-﻿internal class Program
+﻿
+using HospitalUrgencias.Models;
+using Microsoft.VisualBasic;
+
+/// <summary>
+/// Simulates a concurrent medical consultation system.
+/// </summary>
+internal class Program
 {
+    static readonly int totalPatients = 4;
+    static readonly SemaphoreSlim consultSem = new SemaphoreSlim(4);
+    static readonly Random rnd = new Random();
+
+
+    /// <summary>
+    /// Entry point of the program. Simulates patient arrivals.
+    /// </summary>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nPatients are entering the hospital..\n");
+
+        // Simulates the arrival of patients at intervals.
+        for (int i = 1; i <= totalPatients; i++)
+        {
+            int arrivalOrderNum = i;
+
+            Patient patient = new Patient(rnd.Next(1,101), arrivalOrderNum, rnd.Next(5,16));
+
+            Thread patientProccess = new Thread(() => PatientArrival(patient));
+            patientProccess.Start();
+
+            Thread.Sleep(2000);
+        }
+    }
+
+
+    /// <summary>
+    /// Simulates the arrival of a patient and assigns them to a doctor for a consultation.
+    /// </summary>
+    /// <param name="Patient">The patient with all of his properties.</param>
+    private static void PatientArrival(Patient patient)
+    {
+        consultSem.Wait();
+        Doctor assignedDoctor = Doctor.AssignDoctor();
+
+        patient.ChangingPatientStatus(PatientStatus.InConsultation);
+        Thread.Sleep(patient.ConsultationTime * 1000);
+
+        patient.ChangingPatientStatus(PatientStatus.Finished);
+        assignedDoctor.ReleaseDoctor();
+        consultSem.Release();
     }
 }

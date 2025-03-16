eee9dadede4206d5ddeea0f6cb0d0f09706f1316
Sammy <svmmydev@gmail.com>
2025-03-16 19:44:02 +0100

    Refactor and Tarea2 completed

--- Ejercicio1/Tarea2/Program.cs
+++ Ejercicio1/Tarea2/Program.cs
@@ -8,10 +8,12 @@ internal class Program
 {
     static readonly int totalPatients = 4;
     static readonly SemaphoreSlim consultSem = new SemaphoreSlim(4);
-    static readonly object locker = new object();
     static readonly Random rnd = new Random();
 
 
+    /// <summary>
+    /// Entry point of the program. Simulates patient arrivals.
+    /// </summary>
     private static void Main(string[] args)
     {
         Console.WriteLine("Patients are entering the hospital..\n");
@@ -23,10 +25,31 @@ internal class Program
 
             Patient patient = new Patient(rnd.Next(1,101), arrivalPriorityNum, rnd.Next(5,16));
 
-            Thread patient = new Thread(() => PatientArrival(id));
-            patient.Start();
+            Thread patientProccess = new Thread(() => PatientArrival(patient));
+            patientProccess.Start();
 
             Thread.Sleep(2000);
         }
+    }
+
+
+    /// <summary>
+    /// Simulates the arrival of a patient and assigns them to a doctor for a consultation.
+    /// </summary>
+    /// <param name="Patient">The patient with all of his properties.</param>
+    private static void PatientArrival(Patient patient)
+    {
+        Console.WriteLine($"Patient has arrived. ID: {patient.Id} and Priority: {patient.HospitalArrivalPriority}.");
+
+        consultSem.Wait();
+        Doctor assignedDoctor = Doctor.AssignDoctor();
+        Console.WriteLine($"Patient with ID: {patient.Id} is actually entering a consult with Doctor {assignedDoctor.Id}.\n");
+
+        patient.Status = PatientStatus.InConsultation;
+        Thread.Sleep(patient.ConsultationTime * 1000);
+
+        patient.Status = PatientStatus.Finished;
+        Doctor.ReleaseDoctor(assignedDoctor, patient.Id);
+        consultSem.Release();
     }
 }

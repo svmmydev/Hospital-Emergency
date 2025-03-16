c145c443446e9585609378364323609dd478d02c
Sammy <svmmydev@gmail.com>
2025-03-16 02:33:49 +0100

    First refactor made (Patient and Doctor class)

--- Ejercicio1/Tarea2/Program.cs
+++ Ejercicio1/Tarea2/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿
+using hospital_urgencias.Models;
+
+
+internal class Program
 {
+    static readonly int totalPatients = 4;
+    static readonly SemaphoreSlim consultSem = new SemaphoreSlim(4);
+    static readonly object locker = new object();
+    static readonly Random rnd = new Random();
+
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Patients are entering the hospital..\n");
+        
     }
 }
